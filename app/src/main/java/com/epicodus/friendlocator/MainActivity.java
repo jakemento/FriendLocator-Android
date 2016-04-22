d496ec2cfac5b49694a98886894739d06683762a
Jake Steinberg <jakesteinberg@gmail.com>
2016-04-22 09:27:12 -0700

    add activities for find friends and ping my location, as well as implement a click listener interface

--- app/src/main/java/com/epicodus/friendlocator/MainActivity.java
+++ app/src/main/java/com/epicodus/friendlocator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.friendlocator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -11,8 +12,9 @@ import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.question) ImageView mQuestionButton;
+    @Bind(R.id.findFriendsButton) Button mFindFriendsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +22,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mQuestionButton.setOnClickListener(new View.OnClickListener() {
+        mQuestionButton.setOnClickListener(this);
+        mFindFriendsButton.setOnClickListener(this);
+    }
             @Override
             public void onClick(View v) {
+            if (v == mQuestionButton) {
                 Toast questionToast = Toast.makeText(MainActivity.this, "This is an app for locating your friends out in public, allowing you to ping your location and trip details.", Toast.LENGTH_LONG);
                 questionToast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0,0);
                 questionToast.show();
             }
-        });
+                if( v == mFindFriendsButton) {
+                    Intent findFriendsIntent = new Intent(MainActivity.this, FindFriendsActivity.class);
+                    startActivity(findFriendsIntent);
+                }
+        };
 
     }
-}
