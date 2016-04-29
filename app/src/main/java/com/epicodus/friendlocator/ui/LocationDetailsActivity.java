d2ae0223494ca0ceef9d9d3a0a8186ba36a20dda
jake steinberg <jakesteinberg@gmail.com>
2016-04-29 11:51:05 -0700

    add marker for portland to map

--- app/src/main/java/com/epicodus/friendlocator/ui/LocationDetailsActivity.java
+++ app/src/main/java/com/epicodus/friendlocator/ui/LocationDetailsActivity.java
@@ -13,28 +13,35 @@ import com.epicodus.friendlocator.R;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
+import static com.google.android.gms.maps.CameraUpdateFactory.*;
+
 public class LocationDetailsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
     private SupportMapFragment fragment;
+    private CameraUpdate point;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_location_details, container, false);
     }
 
-//    @Bind(R.id.addressTextView) TextView mAddressTextView;
-//    @Bind(R.id.nameTextView) TextView mNameTextView;
-//    @Bind(R.id.detailsTextView) TextView mDetailsTextView;
+    @Bind(R.id.addressTextView) TextView mAddressTextView;
+    @Bind(R.id.nameTextView) TextView mNameTextView;
+    @Bind(R.id.detailsTextView) TextView mDetailsTextView;
 
 
     @Override
@@ -66,16 +73,19 @@ public class LocationDetailsActivity extends FragmentActivity implements OnMapRe
         String name = intent.getStringExtra("inputName");
         String address = intent.getStringExtra("inputAddress");
         String details = intent.getStringExtra("inputDetails");
-//        mAddressTextView.setText(address);
-//        mNameTextView.setText(name);
-//        mDetailsTextView.setText(details);
+        mAddressTextView.setText(address);
+        mNameTextView.setText(name);
+        mDetailsTextView.setText(details);
+        point = CameraUpdateFactory.newLatLng(new LatLng(45, -122));
+
 
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
+        map.moveCamera(point);
         map.addMarker(new MarkerOptions()
-        .position(new LatLng(0,0))
-        .title("Marker"));
+                .position(new LatLng(45, -122)));
+
     }
 }
