7f196f9302732d0b59fa990e0a1e3c7ca2a076ec
Jake Steinberg <jakesteinberg@gmail.com>
2016-05-08 14:00:47 -0700

    add ability to save new user to firebase

--- app/src/main/java/com/epicodus/friendlocator/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/friendlocator/ui/LoginActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.epicodus.friendlocator.R;
-import com.epicodus.friendlocator.models.CreateAccountActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
